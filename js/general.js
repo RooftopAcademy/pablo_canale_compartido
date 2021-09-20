745e7ced85c04b0aaaa0af76484e7eb430ec440f
Pablo Canale <pablocanale.exa@gmail.com>
2021-09-20 01:57:46 -0300

    RT Rebuild in separeted files

--- js/general.js
+++
@@ -1,6 +0,0 @@
-//Show the menu in mobile//
-function toggleMenu() {
-    let nav = document.querySelector('nav');
-    let ul = nav.getElementsByTagName('ul');
-    ul[0].classList.toggle("show");
-}
