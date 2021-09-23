5205388248ea248dd02ddf14af40e6ef3875674f
Pablo Canale <pablocanale.exa@gmail.com>
2021-09-23 11:50:35 -0300

    RT Changes for reformat to ts

---
+++ js/cocktailsDomInit.ts
@@ -0,0 +1,125 @@
+
+
+
+/****************************** DOM ***************************************/
+
+// Listener for login form, register and login buttons//
+
+document.getElementById('login-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (e.submitter.id == 'btn-register') {
+        let name: string = document.getElementById('username').value;
+        let pass: number  = document.getElementById('pass').value;
+        if( !(name === '') && !(pass === undefined)){
+            let user : RegisteredUser = new RegisteredUser();
+            user.name = name;
+            user.pass = pass;
+            bar.addUser(user);
+            bar.messageBox.message = 'Congratulations! You Are Registered';               
+            bar.showBox();
+        }
+        else{
+            bar.messageBox.message = 'Usernama And Password Required For Registration';               
+            bar.showBox();
+        }
+    }
+
+    if (e.submitter.id == 'btn-login') {
+
+        let name = document.getElementById('username').value;
+        let pass = document.getElementById('pass').value;
+        if( !(name === '') && !(pass === '')){
+            if(bar.users.length != 0){
+                // SEGURAMENTE ESTO DE ADEALNTE ESTA MAL, NO DEBERIA LLAMAR A UNA FUNCION GETPASS DESDE ACA, ENCONTRAR UNA FUTURA SOLUCION DESPUES... ZzZzZzzZzZZzzZZ
+                for (let i = 0; i < bar.users.length; i++) {
+                    let user = bar.users[i];
+                    if (user.name == name) {
+                        if (user.pass == pass) {
+                            bar.actualUser= user;                            
+                            document.getElementById('form-cocktails').classList.toggle('hidden-container');
+                            document.getElementById('login-form').classList.toggle('hidden-container');
+                            bar.messageBox.message= `Welcome ${user.name}!`;                            
+                            bar.showBox();                     
+                        }
+                        else{
+                            bar.messageBox.message = `Username Or Password Incorrect!`;                           
+                            bar.showBox(); 
+                            }               
+                    }
+                    else{
+                    bar.messageBox.message= `Username Or Password Incorrect!`;                     
+                    bar.showBox();  
+                    }
+                }
+            }
+            else{
+                bar.messageBox.message= `No Users Registered`;                     
+                bar.showBox();  
+            }    
+        }
+        else{
+            bar.messageBox.message= `Username And Password Required`;                     
+                bar.showBox(); 
+        }
+    }
+})
+
+// listener for form-cocktail, add a cocktail with information provided by the inputs//
+document.getElementById('form-cocktails').addEventListener('submit', function (e) {
+    e.preventDefault();
+    let name = document.getElementById('cocktailName').value;
+    let ingredients = document.getElementById('ingredients').value;
+    let ingredientsArray = ingredients.split(",");
+    let imageUrl = document.getElementById('image').value;
+    let id = bar.getCocktails().length + 1;
+    let cocktail = new Cocktail(name, ingredientsArray, imageUrl);
+    cocktail.setId(id);
+    
+    bar.addCocktail(cocktail);
+})
+
+document.getElementById('btn-favorites').addEventListener('click', function(){
+    if(bar.getActualUser() instanceof (RegisteredUser)){
+        bar.filterFavorites();
+        document.getElementById('btn-all').classList.toggle('hidden-container');
+        document.getElementById('btn-favorites').classList.toggle('hidden-container');
+    }
+    else{
+        bar.messageBox().setMessage(`You Should Be Login For Favorite Option`);                     
+        bar.showBox(); 
+    }    
+})
+ 
+document.getElementById('btn-all').addEventListener('click', function(){
+    bar.showCocktails();
+    document.getElementById('btn-all').classList.toggle('hidden-container');
+    document.getElementById('btn-favorites').classList.toggle('hidden-container');
+});
+
+//My bar object//
+
+const bar = new OwnCocktail();
+
+
+//Just for practice// 
+// https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_daiquiri-1.png    daikiri
+// https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_pina_colada-1.png   pina colada
+// https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_gin_tonic-1.png
+//  https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_tropical_strawberry-1.png   trop Stawberry
+// https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_caipirinha-1.png  caipi
+
+let pinaColada = new Cocktail('Pina Colada', ['White Run', 'Coconut Milk', 'Pineaple Juice', 'Crushed Ice'], 'https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_pina_colada-1.png');
+bar.addCocktail(pinaColada);
+let daikiri = new Cocktail('Daikiri', ['White Run', 'Sugar Syrup', 'Lime Juice', 'Ice Cubes'], 'https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_daiquiri-1.png');
+bar.addCocktail(daikiri);
+
+/*let ginTonic = new Cocktail('Gin Tonic', ['Gin', 'Lime', 'Tonic', 'Ice Cubes'], 'https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_gin_tonic-1.png');
+bar.addCocktail(ginTonic);
+
+let tropStraw = new Cocktail('Trop Strawberry', ['Strawberry', 'Sugar', 'Pineaple', 'Yogurt', 'Milk', 'Ice'], 'https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_tropical_strawberry-1.png');
+bar.addCocktail(tropStraw);
+
+let caipi = new Cocktail('Caipirinha', ['Cachaca', 'Sugar', 'Lime Wedges', 'Crushed Ice'], 'https://images.cocktailflow.com/v1/cocktail/w_300,h_540/cocktail_caipirinha-1.png');
+bar.addCocktail(caipi);
+*/
+
