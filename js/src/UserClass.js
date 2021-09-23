615459eb692806a98d44f9361f613930c6c485ea
Pablo Canale <pablocanale.exa@gmail.com>
2021-09-23 11:52:37 -0300

    RT All reformat to typescryp

--- js/src/UserClass.js
+++
@@ -1,7 +0,0 @@
-// Class for users that are not login in//
-class User {
-    constructor() {
-        this.name = '';
-        this.pass = null;
-    }
-}
--- js/toggleMenu.js
+++
@@ -1,5 +0,0 @@
-//Show the menu in mobile//
-document.getElementsByClassName('arrow-down')[0].addEventListener('click', toggleMenu);
-function toggleMenu() {    
-    document.getElementById('ul-btns').classList.toggle("show");
-}
