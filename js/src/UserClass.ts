e89f56407f417867dbdf673b860642486ea4b02b
Pablo Canale <pablocanale.exa@gmail.com>
2021-09-23 11:48:49 -0300

    RT Changes for reformat to ts

---
+++ js/src/UserClass.ts
@@ -0,0 +1,11 @@
+//80+'9 Class for users that are not login in//
+class User {
+
+    protected _name: string;
+    protected _pass: number;
+    constructor() {
+        this._name = undefined;
+        this._pass = undefined;
+    }
+    
+}
