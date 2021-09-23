11f720c6b0a76e15ad5297c814084d7ed0faaf5d
Pablo Canale <pablocanale.exa@gmail.com>
2021-09-23 11:48:16 -0300

    RT Changes for reformat to ts

---
+++ js/src/MessageBoxClass.ts
@@ -0,0 +1,12 @@
+class MessageBox{
+    private _message: string; 
+    
+    get message(): string{
+        return this._message;
+    }
+    
+    set message(message: string){
+        this._message = message;
+    }    
+
+}
