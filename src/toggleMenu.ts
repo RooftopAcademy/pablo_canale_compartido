862dfbab293ecf90dca63e2c51d7428c718e6f05
Pablo Canale <pablocanale.exa@gmail.com>
2021-09-24 12:43:05 -0300

    RT Fix could be null

--- src/toggleMenu.ts
+++ src/toggleMenu.ts
@@ -1,5 +1,5 @@
 //Show the menu in mobile//
 document.getElementsByClassName('arrow-down')[0].addEventListener('click', toggleMenu);
 function toggleMenu() : void{    
-    document.getElementById('ul-btns').classList.toggle("show");
+    document.getElementById('ul-btns')?.classList.toggle("show");
 }
