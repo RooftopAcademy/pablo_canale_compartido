bf954e1c4983f761002f49d76024d25e7c5e4447
Pablo Canale <pablocanale.exa@gmail.com>
2021-09-26 16:48:22 -0300

    RT added router

--- src/router/index.router.ts
+++ src/router/index.router.ts
@@ -1,13 +1,13 @@
 import { bar } from "../index";
 
 export default function router(route: string){
-    console.log(route);
+    
     switch(route){       
             
-        case '#/explanations':
-            console.log("entro")
+        case '#/explanations':            
             bar.loadContent(route);
             break;
+            
         case '#/cocktails':            
             bar.loadContent(route);
             break;        
