9bd1f4d478bb96b9787dd9757aa9f7e5e5f9c954
Pablo Canale <pablocanale.exa@gmail.com>
2021-09-30 11:58:18 -0300

    RT Fix POST and DELETE

--- src/CocktailService.ts
+++ src/CocktailService.ts
@@ -1,9 +1,12 @@
 import Cocktail from "./CocktailInterface";
-import { bar } from "./index";
 
 export default class CocktailService {
 
     private readonly _urlApi: string = 'https://6148b332035b3600175b9fd8.mockapi.io/Bar/1/';
+
+    get urlApi() {
+        return this._urlApi;
+    }
 
     async postCocktailApi(cocktail: Cocktail) {
         try {
@@ -12,11 +15,9 @@ export default class CocktailService {
                 "headers": { "Content-type": "application/json" },
                 "body": JSON.stringify(cocktail)
             });
-            if (res.ok)
-                bar.cocktails = await this.getCocktailsApi();
         }
         catch (e) {
-            alert("ERROR CATCH POST USER")
+            alert("ERROR CATCH POST COCKTAIL")
         }
     }
 
@@ -41,9 +42,6 @@ export default class CocktailService {
             let res = await fetch(`${this._urlApi}Cocktail/${id}`, {
                 "method": "DELETE"
             });
-
-            if (res.ok)
-                bar.cocktails = await this.getCocktailsApi();
         }
         catch (e) {
             alert("ERROR CATH DELETE COCKTAIL");
